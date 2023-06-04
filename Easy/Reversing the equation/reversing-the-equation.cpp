1309ffdc2afb63d799d7593aaba2e09f72f30873
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2023-06-04 23:47:56 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Reversing the equation/reversing-the-equation.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+{
+  public:
+    string reverseEqn (string s)
+        {
+            //code here.
+            string ans="",temp="";
+        int n=s.size();
+        for(int i=n;i>=0;i--){
+            int a=s[i];
+            if(!(a>=48 && a<=57)) {
+                if(temp.size()!=0){
+                    ans+=temp;
+                }
+            temp="";
+            ans+=s[i];  
+            }
+            else{
+                temp=s[i]+temp;
+            }
+        }
+        if(temp.size()!=0){
+            ans+=temp;
+        }
+    return ans;
+        }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string s; cin >> s;
+        Solution ob;
+        cout <<ob.reverseEqn (s) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
