7653b36c202d604485761210619669c0bc29ebdc
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2023-06-15 07:00:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    string longestPalin (string s) {
+        // code here
+        int n=s.size();
+        string str;
+        str;
+        int ans=0,ansl=0,ansr=0;
+        for(int i=0;i<n;i++){
+            int l=i,r=i;
+            while(s[l]==s[r]&&l>=0&&r<n){
+                if(ans<-l+r+1){
+                    ans=-l+r+1;
+                    ansl=l;ansr=r;
+                }
+                l--;r++;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int l=i,r=i+1;
+            while(s[l]==s[r]&&l>=0&&r<n){
+                if(ans<-l+r+1){
+                    ans=-l+r+1;
+                    ansl=l;ansr=r;
+                    
+                }
+                l--;r++;
+            }
+        }
+        int z=ansl;
+        while(z<=ansr){
+            str+=s[z++];
+        }
+        return str;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        string S; cin >> S;
+        
+        Solution ob;
+        cout << ob.longestPalin (S) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
