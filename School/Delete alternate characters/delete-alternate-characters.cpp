bfe4dd9ef07cf6d1bfc09879e397a4e3e675e447
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2023-07-03 13:35:52 +0530

    Added Solution - GfG to GitHub

---
+++ School/Delete alternate characters/delete-alternate-characters.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+        // code here
+        string s1;
+        for(int i=0;i<S.length();i=i+2){
+            s1.push_back(S[i]);
+        }
+        return s1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
