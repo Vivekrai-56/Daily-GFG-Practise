de691ba04f158d6522c0f6e9da3168069264c500
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2023-08-18 22:14:50 +0530

    Updated Solution - GfG to GitHub

--- Easy/Leaders in an array/leaders-in-an-array.cpp
+++ Easy/Leaders in an array/leaders-in-an-array.cpp
@@ -15,15 +15,17 @@ class Solution{
         // Code here
         vector<int> ans;
         ans.push_back(a[n-1]);
-        int maxi=a[n-1];
+        
+        int maxi = a[n-1];
         for(int i=n-2;i>=0;i--){
             if(a[i]>=maxi){
-                maxi=a[i];
                 ans.push_back(a[i]);
-            }
+                maxi = max(maxi,a[i]);
+            } 
         }
         reverse(ans.begin(),ans.end());
         return ans;
+        
     }
 };
 
