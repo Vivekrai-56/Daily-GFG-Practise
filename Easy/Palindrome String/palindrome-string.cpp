c28a0f4fa8d75033b08d59bcba2406e59485f4c6
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2023-09-21 20:43:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Palindrome String/palindrome-string.cpp
+++ Easy/Palindrome String/palindrome-string.cpp
@@ -10,16 +10,19 @@ class Solution{
 public:	
 	
 	
-	int isPalindrome(string S)
+	int isPalindrome(string s)
 	{
 	    // Your code goes here
-	    int n = S.size();
-        
-        for(int i=0; i<n/2; i++) 
-            if(S[i] != S[n-i-1]) 
-                return 0;
-                
-        return 1;
+	    int n = s.size();
+	    for(int i=0;i<n/2;i++)
+	    {
+	        if(s[i] != s[n-i-1])
+	        {
+	            return false;
+	        }
+	    }
+	    
+	    return true;
 	}
 
 };
