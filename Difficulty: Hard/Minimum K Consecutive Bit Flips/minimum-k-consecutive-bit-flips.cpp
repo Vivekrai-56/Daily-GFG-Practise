48c0947513d070f8c67e1966e47a2128f76fdfe7
Vivek Rai <75068627+Vivekrai-56@users.noreply.github.com>
2025-09-27 07:11:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Minimum K Consecutive Bit Flips/minimum-k-consecutive-bit-flips.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    int kBitFlips(vector<int>& arr, int k) {
+        // code here
+         int n = arr.size();
+        vector<int> isFlipped(n, 0);
+        int flipCount = 0, currFlip = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i >= k) {
+                currFlip ^= isFlipped[i - k]; // remove expired flip
+            }
+            if ((arr[i] ^ currFlip) == 0) {  // still 0 after flips
+                if (i + k > n) return -1;    // can't flip beyond bounds
+                flipCount++;
+                currFlip ^= 1;
+                isFlipped[i] = 1;
+            }
+        }
+        return flipCount;
+    }
+};
